5ccd7c11f02b54ced6453f19eef91db4152af67c
MoizGen1771 <moiz@generex.ai>
2025-07-26 06:36:34 -0400

    allowed host changed

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rsjn_(+ou0tt_-jhuh%o@u_en_)htzifqpn_%=@vk_mzh6-9(+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'django-saas-project-production-2379.up.railway.app',
